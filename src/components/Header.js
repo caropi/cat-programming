a24827a5a794fa6385638109d9e775c0720ece48
caropi <pisanocaroline@gmail.com>
2019-08-06 11:07:12 -0400

    First push of a few components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="App-header">
+            <h1>Well actually...</h1>
+            <h2>Helping cat lovers socialize better in parties</h2>
+        </header>
+    )
+}
+
+export default Header
